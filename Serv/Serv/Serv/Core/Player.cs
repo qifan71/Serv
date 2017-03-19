691d6f28344449d1ece83d41f3d33fcfef86e8e5
QiFan <240136809@qq.com>
2017-03-19 14:29:51 +0800

    no message

---
+++ Serv/Serv/Serv/Core/Player.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class Player
+{
+    public string id;
+    //稍后实现连接类
+    //public Conn conn;
+    //数据
+    public PlayerData data;
+    //临时数据
+    public PlayerTempData tempData;
+}
+
---
+++ Serv/Serv/Serv/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serv
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //DataMgr dataMgr = new DataMgr();
+            ////注册
+            //bool ret = dataMgr.Register("WSHC", "123");
+            //if (ret)
+            //{
+            //    Console.WriteLine("注册成功");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("注册失败");
+            //}
+            ////创建玩家
+            //ret = dataMgr.CreatePlayer("WSHC");
+            //if (ret)
+            //{
+            //    Console.WriteLine("创建玩家成功");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("创建玩家失败");  
+            //}
+            ////获取玩家数据
+            //PlayerData pd = dataMgr.GetPlayerData("WSHL");
+            //if (pd!=null)
+            //{
+            //    Console.WriteLine("获取玩家数据成功 分数是 "+pd.score);
+            //}
+            //else
+            //{
+            //    Console.WriteLine("获取玩家数据失败");
+            //}
+            ////更改玩家数据
+            //pd.score += 10;
+            ////保存数据
+            //Player p = new Player();
+            //p.id = "WSHL";
+            //p.data = pd;
+            //dataMgr.Saveplayer(p);
+            ////重新获取
+            //pd = dataMgr.GetPlayerData("WSHL");
+            //if (pd != null)
+            //{
+            //    Console.WriteLine("获取玩家数据成功 分数是 " + pd.score);
+            //}
+            //else
+            //{
+            //    Console.WriteLine("重新获取玩家数据失败");
+            //}
+            //Console.ReadLine();
+            ServNet servNet = new ServNet();
+            servNet.proto = new ProtocolBytes();
+            servNet.Start("127.0.0.1", 1234);
+            Console.ReadLine();
+        }
+    }
+}
