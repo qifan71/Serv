c2408247ae19bc2fd76964f7f925934b542ded68
QiFan <240136809@qq.com>
2017-03-25 10:48:43 +0800

    no message

--- Client3.20/clientFrame/Assets/ClientNetFrame/Root.cs
+++ Client3.20/clientFrame/Assets/ClientNetFrame/Root.cs
@@ -1,15 +1,18 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Root : MonoBehaviour {
+public class Root : MonoBehaviour 
+{
 
 	// Use this for initialization
-	void Start () {
+	void Start ()
+    {
 	
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void Update () 
+    {
+        NetMgr.Update();
 	}
 }
